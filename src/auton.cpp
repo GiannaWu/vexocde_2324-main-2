18df93afd6c5d35c812deef6a2a8da8b70906edb
vanessatso <98tso.vanessa105@gmail.com>
2024-08-08 09:39:17 +0800

    Update auton.cpp

--- src/auton.cpp
+++ src/auton.cpp
@@ -7,68 +7,146 @@ using namespace auton;
 
 void close_qua(){
 
-//75% 5 pts
+    //75% 5 pts POV red
 
-// setHeading(270);
+    // setHeading(270);
 
-// driveAndTurn(-1.1, 0);
-// clip.set(true);
-// turnToAngle(-106);
-// inta.spin(reverse, 100, pct);
-// driveAndTurn(1, -90);
-// turnToAngle(60);
-// intakeLift.set(true);
-// driveAndTurn(1.7, 63, 80, 80);
-// driveAndTurn(0.4, 63);
-// intakeLift.set(false);
-// turnToAngle(150);
+    // driveAndTurn(-1.1, 0);
+    // clip.set(true);
 
-// intakeLift.set(true);
-// inta.stop(brake);
-// driveAndTurn(0.7, 150);
-// clip.set(false);
+    // inta.spin(reverse, 100, pct);
+    // turnToAngle(-106);
+    // driveAndTurn(1, -90);
+    // turnToAngle(60);
+    // intakeLift.set(true);
+    // inta.spin(reverse, 100, pct);
 
-///////////////////////////////////////
+    // driveAndTurn(2.1, 63, 80, 80);
+    // // driveAndTurn(1.7, 63, 80, 80);
+    // // driveAndTurn(0.4, 63);
+    // intakeLift.set(false);
+    // inta.spin(reverse, 100, pct);
+    // turnToAngle(150);
 
-//100% 
+    // driveAndTurn(0.7, 150);
+    // intakeLift.set(true);
+    // clip.set(false);
+    // inta.stop(brake);
 
-setHeading(290);
-driveAndTurn(-2.28, 0);
-clip.set(true);
-// turnToAngle(30);
-// driveAndTurn(-0.43, 30);
-// clip.set(true);
-// turnToAngle(-20);
-// inta.spin(reverse, 100, pct);
-// driveAndTurn(0.7, 0);
-// clip.set(true);
+    ///////////////////////////////////////
 
+    //100% 7pts POV red 
 
+    // setHeading(290);
+    // driveAndTurn(-2.28, 0);
+    // clip.set(true);
+    // inta.spin(reverse, 100, pct);
+    // turnToAngle(-40);
+    // driveAndTurn(0.46, 0);
+    // clip.set(false);
 
-// driveAndTurn(0.2, 0);
-// turnToAngle(80);
-// driveAndTurn(1, 0);
+    // driveAndTurn(1.7, 63, 80, 80);
+    // driveAndTurn(0.4, 63);
+    // intakeLift.set(false);
+    // turnToAngle(150);
+    // intakeLift.set(true);
+    // turnToAngle(0); //change value
 
-// driveAndTurn(0.35,270);
-// driveAndTurn(2,340);
+    // inta.stop(brake);
+    // driveAndTurn(0.9, 128);
+    // clip.set(true);
+    // inta.spin(reverse, 100, pct);
 
-// pner.spin(fwd,100,pct);
-// wait(1000, msec);
-// pner.stop(hold);
+    // driveAndTurn(0.7, 0); //change value
 
-// inta.spin(fwd,100,pct);
-// driveAndTurn(2.3, 28,100,50);
-// turnToAngle(0);
-// driveAndTurn(-3, 43,55,100);
-// turnToAngle(135);
+    ///////////////////////////////////////
 
-// clip.set(true);
-// turnToAngle(-270);
-// turnToAngle(135);
-// clip.set(false);
-// driveAndTurn(1.3, 90, 100, 87);
-// inta.stop(brake);
-// driveAndTurn(0.8,90);
-// inta.spin(reverse,100,pct);
+    //75% 5 pts POV blue
 
+    // setHeading(90);
+
+    // driveAndTurn(-1.1, 0);
+    // clip.set(true);
+
+    // inta.spin(reverse, 100, pct);
+    // turnToAngle(106);
+    // driveAndTurn(1, 90);
+    // turnToAngle(-60);
+    // intakeLift.set(true);
+    // inta.spin(reverse, 100, pct);
+
+    // driveAndTurn(2.1, -63, 80, 80);
+    // intakeLift.set(false);
+    // inta.spin(reverse, 100, pct);
+    // turnToAngle(-150);
+
+    // driveAndTurn(0.6, -150);
+    // intakeLift.set(true);
+    // clip.set(false);
+    // inta.stop(brake);
+
+    ///////////////////////////////////////
+
+    // turnToAngle(30);
+    // driveAndTurn(-0.43, 30);
+    // clip.set(true);
+    // turnToAngle(-20);
+    // inta.spin(reverse, 100, pct);
+    // driveAndTurn(0.7, 0);
+    // clip.set(true);
+
+    // driveAndTurn(0.2, 0);
+    // turnToAngle(80);
+    // driveAndTurn(1, 0);
+
+    // driveAndTurn(0.35,270);
+    // driveAndTurn(2,340);
+
+    // pner.spin(fwd,100,pct);
+    // wait(1000, msec);
+    // pner.stop(hold);
+
+    // inta.spin(fwd,100,pct);
+    // driveAndTurn(2.3, 28,100,50);
+    // turnToAngle(0);
+    // driveAndTurn(-3, 43,55,100);
+    // turnToAngle(135);
+
+    // clip.set(true);
+    // turnToAngle(-270);
+    // turnToAngle(135);
+    // clip.set(false);
+    // driveAndTurn(1.3, 90, 100, 87);
+    // inta.stop(brake);
+    // driveAndTurn(0.8,90);
+    // inta.spin(reverse,100,pct);
+
+    ///////////////////////////////////////
+
+    //75% 6pts POV red 
+    //write in far_qua later
+
+    setHeading(270);
+
+    driveAndTurn(-1.1, 0);
+    clip.set(true);
+
+    inta.spin(reverse, 100, pct);
+    turnToAngle(148);
+    driveAndTurn(0.99, 150);
+    turnToAngle(88, 100);
+    driveAndTurn(1, 88, 100, 100);
+
+    inta.spin(reverse, 100, pct);
+    driveAndTurn(-1.2, 110);
+    
+    turnToAngle(55);
+    inta.spin(reverse, 100, pct);
+    driveAndTurn(0.7, 60, 100, 100);
+
+    // turnToAngle(-220);
+    // driveAndTurn(1, -220);
+    // intakeLift.set(true);
+    // clip.set(false);
+    // inta.stop(brake);
 }
