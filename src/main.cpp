51146003c19ef1f59704e035aae8873c3fb2131a
vanessatso <98tso.vanessa105@gmail.com>
2024-09-27 21:50:55 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "main.h"
 #include "autons/auton.h"
 #include "autons/auton_functions.h"
+#include "utility/buttons.h"
 using namespace vex;
 using namespace auton;
 competition Competition;
@@ -22,6 +23,8 @@ int autonoption = 0;
 
 bool isAutonRunning = false;
 
+int pick = 0;
+
 float deadband(float input, float width){
   if (fabs(input)<width){
     return(0);
@@ -36,24 +39,34 @@ void pre_auton(void) {
     wait(10, msec);
   }
   // heading_convert(bob.heading());
-  while(1){
-  if (Brain.Screen.pressing()){
-    int X = Brain.Screen.xPosition();
-    int Y = Brain.Screen.xPosition();
-    if ((Y >= 120)){
-      Brain.Screen.clearScreen();
-      Brain.Screen.setFillColor(red);
-      Brain.Screen.printAt(180,136, "Close Auton");
-      autonoption = 1;
 
-    } else if(Y <= 120){
-      Brain.Screen.clearScreen();
-      Brain.Screen.drawRectangle(0,0,480,272);
-      Brain.Screen.setFillColor(red);
-      Brain.Screen.printAt(190,136, "Far Auton");
-      autonoption = 2;
+  GuiButton *pickAutonButtons[] = {
+    // new GuiButton(0, 0, 480, 240, color(0, 0, 0), color(255, 0, 0)),
+    new GuiButton(0, 0, 240, 120, color(0, 0, 0), color(255, 0, 0), "red left"),
+    new GuiButton(240, 0, 240, 120, color(0, 0, 0), color(0, 255, 0), "red right"),
+    new GuiButton(0, 120, 240, 120, color(0, 0, 0), color(0, 0, 255), "blue left"),
+    new GuiButton(240, 120, 240, 120, color(0, 0, 0), color(255, 255, 0), "blue right"),
+  };
+
+  while(1) {
+    
+    // Detect
+    for (int i = 0; i < 4; i++) {
+      GuiButton *button = pickAutonButtons[i];
+      // printf("press: %d\n", button -> isPressing());
+      if (button -> isPressing()) {
+        pick = i + 1;
+        button -> setSelected(true);
+      } else {
+        button -> setSelected(false);
+      }
     }
-  }
+
+    // Draw
+    for (GuiButton *button : pickAutonButtons) {
+      button -> draw();
+    }
+
     wait(20, msec);
 
   }
@@ -62,7 +75,6 @@ void pre_auton(void) {
 
 void autonomous(void) {
   isAutonRunning = true;
-  blue_left();
   // test();
   Brain.Screen.clearScreen();
   Brain.Screen.setFillColor(red);
@@ -72,6 +84,19 @@ void autonomous(void) {
   // } else if(autonoption == 1){
 
   // }
+  if(pick == 1){
+    red_left();
+  }
+  if(pick == 2){
+    red_right();
+  }
+  if(pick == 3){
+    blue_left();
+  }
+  if(pick == 4){
+    blue_right();
+  }
+  blue_left();
   red_left();
   blue_right();
   red_right();
