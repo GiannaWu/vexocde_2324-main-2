5fb61796916fe7888023bd3c9341cabdd0647549
GiannaWu <166489574+GiannaWu@users.noreply.github.com>
2024-09-28 11:41:55 +0800

    Update buttons.h

--- include/utility/buttons.h
+++ include/utility/buttons.h
@@ -1,20 +1,3 @@
 #pragma once
 
-#include "main.h"
-
 int autonpick();
-
-class GuiButton {
-    public:
-        GuiButton(int x, int y, int width, int height, color background_color, color selected_color, char *text);
-        bool containsPosition(int posX, int posY);
-        bool isPressing();
-        void setSelected(bool state);
-        void draw();
-    private:
-        int x, y;
-        int width, height;
-        color background_color, selected_color;
-        color display_color;
-        char *text;
-};
