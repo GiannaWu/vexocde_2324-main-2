f182ff2419aa27dd2cec160678412baf9dc7e30d
vanessatso <98tso.vanessa105@gmail.com>
2024-10-03 14:03:45 +0800

    Create auton_skills.cpp

---
+++ src/auton_skills.cpp
@@ -0,0 +1,85 @@
+#include "robot-config.h"
+#include "autons/auton.h"
+#include "main.h"
+#include "autons/auton_functions.h"
+using namespace vex;
+using namespace auton;
+
+void auton_skills(){
+    setHeading(0);
+
+    //implement lift the arm
+    driveAndTurn(0, 0);
+    intamo.spin(fwd, 100, pct);
+    //implement lower the arm
+    driveAndTurn(-0, 0);
+
+    turnToAngle(0);
+    driveAndTurn(-0, 0);
+    clamp.set(true);
+    turnToAngle(0);
+    
+    intamo.spin(reverse, 100, pct);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(-0, 0);
+    clamp.set(false);
+
+    driveAndTurn(0, 0);
+    turnToAngle(180);
+    driveAndTurn(-0, 0);
+    clamp.set(true);
+
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(0, 0);
+    driveAndTurn(-0, 0);
+    clamp.set(false);
+
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    intamo.stop(brake);
+
+    turnToAngle(-0);
+    driveAndTurn(-0, 0);
+    clamp.set(true);
+    turnToAngle(-0);
+
+    driveAndTurn(0, 0);
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(-0);
+    driveAndTurn(0, 0);
+    turnToAngle(90);
+    driveAndTurn(0, 90);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    //implement go to the corner and remove the blue ring 
+    clamp.set(false);
+
+    //if enough time, go score another wall stake ring
+    
+    //hang
+    turnToAngle(0);
+    driveAndTurn(0, 0);
+    //implement hang
+    //finish
+}
