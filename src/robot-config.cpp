e31672de44848285d577025a9fb09f4d1913600f
vanessatso <98tso.vanessa105@gmail.com>
2024-11-08 13:38:12 +0800

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -11,24 +11,23 @@ brain  Brain;
 controller con;
 motor l1 = motor(PORT1, ratio6_1, 1);
 motor l2 = motor(PORT15, ratio6_1, 1);
-motor l3 = motor(PORT10, ratio6_1, 1);
+motor l3 = motor(PORT20, ratio6_1, 1);
 motor_group leftmo = motor_group(l1, l2, l3);
 motor r1 = motor(PORT4, ratio6_1, 0);
 motor r2 = motor(PORT8, ratio6_1, 0);
 motor r3 = motor(PORT6, ratio6_1, 0);
 motor_group rightmo = motor_group(r1, r2, r3);
-inertial bob = inertial(PORT14);
-motor inta1 = motor(PORT7, ratio6_1, 0); //right
-motor inta2 = motor(PORT2, ratio6_1, 0); //left
+inertial bob = inertial(PORT9);
+motor inta1 = motor(PORT7, ratio6_1, 0);
+motor inta2 = motor(PORT2, ratio6_1, 0);
 motor_group intamo = motor_group(inta1, inta2);
 //motor pner = motor(PORT13, ratio18_1, 0);
-digital_out clamp = digital_out(Brain.ThreeWirePort.H);
-digital_out elevation = digital_out(Brain.ThreeWirePort.F);
+digital_out clamp = digital_out(Brain.ThreeWirePort.A);
 //digital_out hood = digital_out(Brain.ThreeWirePort.D);
-digital_out intakeLift = digital_out(Brain.ThreeWirePort.G);
+digital_out intakeLift = digital_out(Brain.ThreeWirePort.H);
 distance tim = distance(PORT17);
-rotation ForwardTracker = rotation(PORT12, false);
-rotation SidewayTracker = rotation(PORT11, false);
+rotation ForwardTracker = rotation(PORT11, true);
+rotation SidewayTracker = rotation(PORT19, false);
 //vision bakchu = vision(PORT7);
 // VEXcode generated functions
 
