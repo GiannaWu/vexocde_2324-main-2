567413e5f69bf13f404f61702debaa4dd6d82923
vanessatso <98tso.vanessa105@gmail.com>
2024-11-15 17:46:03 +0800

    Add files via upload

---
+++ include/auton.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "JAR-Template/drive.h"
+
+class Drive;
+
+extern Drive chassis;
+
+void default_constants();
+void blue_left();
+void red_left();
+void blue_right();
+void red_right();
+void drive_test();
